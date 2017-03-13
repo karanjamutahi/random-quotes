2201be024f8ffaaa71e097c06162f3f78ac8e19d
karanjamutahi <mutahi.henry@gmail.com>
2017-03-13 17:51:32 +0300

    Working.Bugs on the Tweet Button

--- goodStuff.js
+++ goodStuff.js
@@ -6,7 +6,17 @@ $(document).ready(function(){
 
     $('#getQuote').on('click', function(){
     $.getJSON("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=",function(quoter){
+        var quoteActual = quoter[0].content;
+        var tweetQuote = quoteActual.replace(/^([a-z]|[A-Z]|[0-9]|\s)|<p>|<\/p>/g,"");
+        var tweetQuoteEdit = tweetQuote.replace(/\s/g,"+");
+        var quoteAuthor = quoter[0].title;
     $('#quote-text').html(quoter[0].content); $('#author').html(quoter[0].title);
-    });
+     var pathname = window.location;
+     var tweeturl = "http://twiter.com/share?url="+encodeURI(pathname)+"&text="+tweetQuoteEdit;
+    $('#tweetLink').attr("href", tweeturl);
+    console.log(tweetQuoteEdit);
+    console.log(pathname);
+    console.log(tweeturl);
+     });
     });
 });
